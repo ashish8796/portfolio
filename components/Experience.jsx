f7f642424c2addf4408e9469ca32d93012b8a808
ashish8796 <ashishsaini.ak@gmail.com>
2024-07-09 17:53:55 +0530

    experience is being added

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,35 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 export default function Experience() {
+  const [isExperience, setIsExperience] = useState("gutesleben");
   const experiences = [
     {
       title: "Fullstack Developer",
       companyName: "Gutes Leben",
       tenure: "May 2023 - Present",
-      description: "",
+      companyTag: "gutesleben",
+      description: [
+        "Upgraded React Native to the latest version, optimizing performance with Flashlight and react-native-fast-image.",
+        "Enhanced app performance through memoization and useCallback to minimize unnecessary re-renders.",
+        "Implemented CI/CD pipelines using Expo EAS builds for streamlined deployment across platforms.",
+        "Migrated CMS, a static React app to Next.js 13 for improved SSR and SSG capabilities.",
+        "Optimized CMS performance, achieving faster load times and enhanced SEO.",
+        "Developed APIs to extend functionality for CMS and mobile app features.",
+        "Refactored large APIs into modular components for improved performance and maintainability.",
+        "Managed successful migration from Azure to AWS, enhancing system scalability and reliability.",
+      ],
     },
     {
       title: "SDE I",
       companyName: "Meesho",
+      companyTag: "meesho",
       tenure: "Sep 2021 - Apr 2023",
-      description: "",
+      description: [
+        "Integrated Mini-Games in the community feature on the App, utilizing webview to enhance user engagement and functionality.",
+        "Enabled e-commerce on WhatsApp, expanding the reach to millions of users and driving significant growth in sales.",
+        "Implemented notifications on the web, improving real-time communication and user experience.",
+        "Analyzed and Worked with analytics tools (Mixpanel, Clevertap, GA4), contributing to data-driven decision-making and strategic planning.",
+      ],
     },
     {
       title: "Intern",
       companyName: "Masai School",
       tenure: "Jan 2021 - Sep 2021",
-      description: "",
+      companyTag: "masai",
+      description: [
+        "Learned and Mastered important concepts in programming, building a strong foundation for a successful career in software development.",
+        "Collaborated in building Masai School platform, contributing to design, development, and enhancement of user-centric features.",
+      ],
     },
   ];
 
+  const handleCollapseButton = (companyTag) => {
+    setIsExperience(companyTag);
+  };
+
   return (
-    <div>
+    <div className="flex flex-col py-2 xl:py-4 w-full gap-y-2 sm:gap-y-4 text-start">
       {experiences.map((experience, exp_id) => (
-        <div>
-          <h2>{experience.title}</h2>
-          <h2>
+        <div className="flex flex-col flex-wrap w-full p-4" key={exp_id}>
+          <div className="flex justify-between">
+            <h2 className="text-lg font-semibold">{experience.title}</h2>
+            <button
+              onClick={() => {
+                handleCollapseButton(experience.companyTag);
+              }}
+            >
+              icon
+            </button>
+          </div>
+
+          <h2 className="text-md">
             {experience.companyName} - {experience.tenure}
           </h2>
+
+          <p className="text-base w-full break-words">
+            {isExperience == experience.companyTag && (
+              <motion.ul
+                variants={fadeIn("left", 0.4)}
+                animate="show"
+                exit="hidden"
+                className="pt-2 list-disc list-inside"
+              >
+                {experience.description.map((point, pointId) => (
+                  <li key={pointId} className="text-[14px]">
+                    {point}
+                  </li>
+                ))}
+              </motion.ul>
+            )}
+          </p>
         </div>
       ))}
     </div>
--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,120 +1,16 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
-import {
-  FaAws,
-  FaCss3,
-  FaGit,
-  FaHtml5,
-  FaJava,
-  FaJs,
-  FaNodeJs,
-  FaReact,
-} from "react-icons/fa";
-import {
-  SiExpress,
-  SiMongodb,
-  SiMongoose,
-  SiNextdotjs,
-  SiPostgresql,
-  SiSpringboot,
-  SiTypescript,
-} from "react-icons/si";
-import { TbBrandReactNative, TbSql } from "react-icons/tb";
 
 import { fadeIn } from "../../variants";
 import Skills from "../../components/Skills";
-
-//  data
-export const aboutData = [
-  {
-    title: "skills",
-    info: [
-      {
-        title: "Frontend Development",
-        skills: [
-          { name: "HTML", Icon: FaHtml5 },
-          { name: "Css", Icon: FaCss3 },
-          { name: "Javascript", Icon: FaJs },
-          { name: "Typescript", Icon: SiTypescript },
-          { name: "React Js", Icon: FaReact },
-          { name: "React Native", Icon: TbBrandReactNative },
-          { name: "Next Js", Icon: SiNextdotjs },
-        ],
-      },
-
-      {
-        title: "Backend Development",
-        skills: [
-          { name: "Node Js", Icon: FaNodeJs },
-          { name: "Express Js", Icon: SiExpress },
-          { name: "Java", Icon: FaJava },
-          { name: "Sprint Boot", Icon: SiSpringboot },
-        ],
-      },
-
-      {
-        title: "Database",
-        skills: [
-          { name: "Mongoose", Icon: SiMongoose },
-          { name: "MongoDB", Icon: SiMongodb },
-          { name: "SQL", Icon: TbSql },
-          { name: "PostgreSQL", Icon: SiPostgresql },
-        ],
-      },
-
-      {
-        title: "Cloud",
-        skills: [{ name: "AWS", Icon: FaAws }],
-      },
-
-      {
-        title: "Other",
-        skills: [{ name: "Git", Icon: FaGit }],
-      },
-    ],
-  },
-  {
-    title: "experience",
-    info: [
-      {
-        title: "UX/UI Designer - XYZ Company",
-        stage: "2012 - 2023",
-      },
-      {
-        title: "Web Developer - ABC Agency",
-        stage: "2010 - 2012",
-      },
-      {
-        title: "Intern - DEF Corporation",
-        stage: "2008 - 2010",
-      },
-    ],
-  },
-  // {
-  //   title: "credentials",
-  //   info: [
-  //     {
-  //       title: "Web Development - ABC University, LA, CA",
-  //       stage: "2011",
-  //     },
-  //     {
-  //       title: "Computer Science Diploma - AV Technical Institute",
-  //       stage: "2009",
-  //     },
-  //     {
-  //       title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
-  //       stage: "2006",
-  //     },
-  //   ],
-  // },
-];
+import Experience from "../../components/Experience";
 
 const About = () => {
   const [tab, setTab] = useState("skill");
 
   return (
-    <div className="h-full bg-primary/30 xl:py-32 py-40 text-center xl:text-left xl:mt-12">
-      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+    <div className="bg-primary/30 xl:py-32 py-40 text-center xl:text-left">
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6 xl:mt-12">
         {/* info */}
         <motion.div
           variants={fadeIn("left", 0.4)}
@@ -148,7 +44,7 @@ const About = () => {
 
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {tab === "skill" && <Skills />}
-            {tab === "experience" && <Skills />}
+            {tab === "experience" && <Experience />}
           </div>
         </motion.div>
       </div>
